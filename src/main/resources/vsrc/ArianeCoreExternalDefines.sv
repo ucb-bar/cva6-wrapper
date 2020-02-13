04c7eef74d7da15566014e2424be188749b664b0
abejgonzalez <abe.j.gonza@gmail.com>
2020-02-12 17:19:32 -0800

    [ariane] change blackbox ordering | have defines in code | bump ariane for ifdef

---
+++ src/main/resources/vsrc/ArianeCoreExternalDefines.sv
@@ -0,0 +1,6 @@
+// ******************************************************************
+// Specific defines needed for building
+// ******************************************************************
+
+`define WT_DCACHE // use the write-through cache by default
+`define DISABLE_TRACER // remove the default tracer widget
