e02436d2aaf934b6e58d5e11e87276ba0d840f2a
abejgonzalez <abe.j.gonza@gmail.com>
2020-03-23 16:33:57 -0700

    change variable naming

--- ariane.mk
+++ ariane.mk
@@ -6,5 +6,5 @@
 # THE FOLLOWING MUST BE += operators
 ##################################################################
 
-# sourced used to run the generator
-EXTRA_GENERATOR_SOURCES += $(call lookup_srcs,$(base_dir)/generators/ariane,sv) $(call lookup_srcs,$(base_dir)/generators/ariane,v)
+# requirements needed to run the generator
+EXTRA_GENERATOR_REQS += $(call lookup_srcs,$(base_dir)/generators/ariane,sv) $(call lookup_srcs,$(base_dir)/generators/ariane,v)
