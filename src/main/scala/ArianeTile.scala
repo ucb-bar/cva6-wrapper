bfcdd49908ccd853bfa968c7d03fb3fb594c8a2c
David Biancolin <david.biancolin@gmail.com>
2020-04-09 14:25:17 -0700

    Fix trace instruction bit indexing

--- src/main/scala/ArianeTile.scala
+++ src/main/scala/ArianeTile.scala
@@ -269,17 +269,16 @@ class ArianeTileModuleImp(outer: ArianeTile) extends BaseTileModuleImp(outer){
     // unpack the trace io from a UInt into Vec(TracedInstructions)
     //outer.traceSourceNode.bundle <> core.io.trace_o.asTypeOf(outer.traceSourceNode.bundle)
 
+    // Abe...
     for (w <- 0 until outer.arianeParams.core.retireWidth) {
-      outer.traceSourceNode.bundle(w).clock     := core.io.trace_o(traceInstSz*w + 0).asClock
-      outer.traceSourceNode.bundle(w).reset     := core.io.trace_o(traceInstSz*w + 1)
-      outer.traceSourceNode.bundle(w).valid     := core.io.trace_o(traceInstSz*w + 2)
-      outer.traceSourceNode.bundle(w).iaddr     := core.io.trace_o(traceInstSz*w + 42, traceInstSz*w + 3)
-      outer.traceSourceNode.bundle(w).insn      := core.io.trace_o(traceInstSz*w + 74, traceInstSz*w + 43)
-      outer.traceSourceNode.bundle(w).priv      := core.io.trace_o(traceInstSz*w + 77, traceInstSz*w + 75)
-      outer.traceSourceNode.bundle(w).exception := core.io.trace_o(traceInstSz*w + 78)
-      outer.traceSourceNode.bundle(w).interrupt := core.io.trace_o(traceInstSz*w + 79)
-      outer.traceSourceNode.bundle(w).cause     := core.io.trace_o(traceInstSz*w + 87, traceInstSz*w + 80)
-      outer.traceSourceNode.bundle(w).tval      := core.io.trace_o(traceInstSz*w + 127, traceInstSz*w + 88)
+      outer.traceSourceNode.bundle(w).valid     := core.io.trace_o(traceInstSz*w + 0 )
+      outer.traceSourceNode.bundle(w).iaddr     := core.io.trace_o(traceInstSz*w + 40, traceInstSz*w + 1)
+      outer.traceSourceNode.bundle(w).insn      := core.io.trace_o(traceInstSz*w + 72, traceInstSz*w + 41)
+      outer.traceSourceNode.bundle(w).priv      := core.io.trace_o(traceInstSz*w + 75, traceInstSz*w + 73)
+      outer.traceSourceNode.bundle(w).exception := core.io.trace_o(traceInstSz*w + 76)
+      outer.traceSourceNode.bundle(w).interrupt := core.io.trace_o(traceInstSz*w + 77)
+      outer.traceSourceNode.bundle(w).cause     := core.io.trace_o(traceInstSz*w + 85, traceInstSz*w + 78)
+      outer.traceSourceNode.bundle(w).tval      := core.io.trace_o(traceInstSz*w + 125, traceInstSz*w + 86)
     }
   } else {
     outer.traceSourceNode.bundle := DontCare
