8b4cc06285dcec2a69556bab839e567b3e3a887f
franout <francesco.angione@polito.it>
2026-06-17 12:45:56 +0200

    fix: moved extra sim source in internal makefile, sourced only when needed

--- chipyard.mk
+++ chipyard.mk
@@ -8,5 +8,3 @@
 
 # requirements needed to run the generator
 EXTRA_GENERATOR_REQS += $(call lookup_srcs,$(base_dir)/generators/cva6,sv) $(call lookup_srcs,$(base_dir)/generators/cva6,v)
-
-EXTRA_SIM_SOURCES += $(base_dir)/generators/cva6/src/main/resources/cva6/vsrc/memories.sv
